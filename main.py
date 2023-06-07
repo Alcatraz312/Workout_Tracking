6339362e8bc71daf4b1109d8115ae4637e910771
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-06-07 21:15:09 +0530

    new commit

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import datetime
 import json
 from keys import *
 
+exercise = input()
+
 api_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
+headers = {
+    "x-app-id" : APP_ID,
+    "x-app_key" : API_KEY
+}
 
+params = {
+    "query" : exercise,
+    
+
+}
