b25bf9a13b5b4868754cb44752b80e671a1151e4
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-06-07 21:18:48 +0530

    very new commit

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ headers = {
 
 params = {
     "query" : exercise,
-    
+    "gender" : "male",
+    "weight_kg" : "60",
+    "height_cm" : "182.88",
+    "age" : "18"
 
 }
+
+response = requests.post(api_endpoint, json=params, headers=headers)
+result = response.json()
+print(result)
+
