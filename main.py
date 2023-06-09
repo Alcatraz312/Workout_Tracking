2bb4d15544f65d093288bf0a95ca9cf215c06a89
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-06-09 23:56:51 +0530

    new commit

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ params = {
     "weight_kg" : "60",
     "height_cm" : "182.88",
     "age" : "18"
-    
+
 }
 
 response = requests.post(api_endpoint, json=params, headers=headers)
 result = response.json()
+print(result)
 
 
